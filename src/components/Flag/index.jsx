e77ecfb3044b1429b0a270a89629f36eed04be5d
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-09-27 13:17:21 +0800

    test

--- src/components/Flag/index.jsx
+++ src/components/Flag/index.jsx
@@ -1,31 +1,51 @@
+// // 模組樣式
+// import S from './style.module.css'
+// // json檔
+// import flagsData from './flags.json'
+
+// // 首頁
+// function Flag({ countryCode }) {
+//   const flag = flagsData.flags.find((flag) => flag.countryCode === countryCode)
+
+//   let position
+//   let height
+
+//   if (flag) {
+//     position = flag.position
+//     height = flag.height
+//   } else {
+//     position = '-3073'
+//     height = '10'
+//   }
+
+//   return (
+//     <div
+//       className={S.flag}
+//       style={{
+//         height: `${height}px`,
+//         backgroundPosition: `${position}px 0`
+//       }}
+//     ></div>
+//   )
+// }
+
+// export default Flag
+
 // 模組樣式
 import S from './style.module.css'
-// json檔
-import flagsData from './flags.json'
+// library
+import CountryFlag from 'react-country-flag'
 
 // 首頁
 function Flag({ countryCode }) {
-  const flag = flagsData.flags.find((flag) => flag.countryCode === countryCode)
-
-  let position
-  let height
-
-  if (flag) {
-    position = flag.position
-    height = flag.height
-  } else {
-    position = '-3073'
-    height = '10'
-  }
-
   return (
-    <div
-      className={S.flag}
+    <CountryFlag
+      countryCode={countryCode.toUpperCase()}
+      svg
       style={{
-        height: `${height}px`,
-        backgroundPosition: `${position}px 0`
+        width: '20px'
       }}
-    ></div>
+    />
   )
 }
 
